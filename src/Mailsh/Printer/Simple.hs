a172703bb5d6abe98da94e81279f9695ad88eab3
Philip Kranz <philip.kranz@googlemail.com>
2016-09-23 11:05:40 +0200

    Mailsh.Printer.Simple: Use w3m to view html mails.

--- src/Mailsh/Printer/Simple.hs
+++ src/Mailsh/Printer/Simple.hs
@@ -7,6 +7,8 @@ import qualified Pipes.Prelude as P
 import qualified Pipes.Parse as PP
 import qualified Data.ByteString as B
 import Data.List
+import System.Process
+import System.IO
 
 import Mailsh.Printer.Utils
 import Mailsh.Message
@@ -28,4 +30,12 @@ textPlainPrinter :: PP.Parser B.ByteString IO ()
 textPlainPrinter = utf8decoder
 
 textHtmlPrinter :: PP.Parser B.ByteString IO ()
-textHtmlPrinter = liftIO $ putStrLn "HTML message not shown"
+textHtmlPrinter = do
+  (Just inH, _, _, procH) <- liftIO $
+    createProcess_ "html viewer" (shell "w3m -T text/html") { std_in = CreatePipe }
+  feedToHandle inH
+  void $ liftIO $ waitForProcess procH
+    where
+      feedToHandle h = PP.foldAllM (const (liftIO . B.hPut h))
+                                   (return ())
+                                   (const (hClose h))
