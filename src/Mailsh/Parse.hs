4865448c19bc479a67997f04d7fa14dab8fbc528
Philip Kranz <philip.kranz@googlemail.com>
2016-10-02 16:37:53 +0200

    Mailsh.Parse: Fix newlines

--- src/Mailsh/Parse.hs
+++ src/Mailsh/Parse.hs
@@ -4,6 +4,11 @@ module Mailsh.Parse
 
 import Data.Attoparsec.ByteString.Lazy
 import qualified Data.ByteString.Lazy as B
+import Data.Word
 
 parseFile :: FilePath -> Parser a -> IO (Maybe a)
-parseFile fp p = maybeResult <$> parse p <$> B.readFile fp
+parseFile fp p = maybeResult <$> parse p <$> B.concatMap fixCrlfL <$> B.readFile fp
+
+fixCrlfL :: Word8 -> B.ByteString
+fixCrlfL 10 = B.pack [13, 10]
+fixCrlfL x  = B.pack [x]
