57d67b1593416d2233dab9fede5873a5c2e06c08
Philip Kranz <philip.kranz@googlemail.com>
2016-10-06 12:17:18 +0200

    Disable the 'seePrinter' for now, since 'see' is broken on my system.

--- src/Mailsh/Printer/Simple.hs
+++ src/Mailsh/Printer/Simple.hs
@@ -25,12 +25,28 @@ simplePrinter = do
   liftIO $ putStrLn $ formatHeaders filter hs
   case simpleContentType <$> listToMaybe (lookupOptionalField "Content-Type" hs) of
     Nothing -> textPlainPrinter
-    Just ct -> seePrinter ct
+    Just ct -> mimePrinter ct
+
+mimePrinter :: String -> Printer' ()
+mimePrinter mimeType = case mimeType of
+                        "text/html" -> w3mPrinter
+                        _           -> textPlainPrinter
 
 seePrinter :: String -> Printer' ()
 seePrinter mimeType = do
   (Just inH, _, _, procH) <- liftIO $
     createProcess_ "see" (shell ("see " ++ mimeType ++ ":-")) { std_in = CreatePipe }
+  feedToHandle inH
+  void $ liftIO $ waitForProcess procH
+    where
+      feedToHandle h = PP.foldAllM (const (liftIO . B.hPut h))
+                                   (return ())
+                                   (const (liftIO (hClose h)))
+
+w3mPrinter :: Printer' ()
+w3mPrinter = do
+  (Just inH, _, _, procH) <- liftIO $
+    createProcess_ "see" (shell "w3m | cat") { std_in = CreatePipe }
   feedToHandle inH
   void $ liftIO $ waitForProcess procH
     where
