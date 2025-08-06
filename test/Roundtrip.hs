ad8f16ac8d8ecb3979a16d47b48d0c89cedd8e66
Philip Kranz <pk@pmlk.net>
2025-08-06 20:35:46 +0200

    Add executable 'roundtrip'

---
+++ test/Roundtrip.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString as BS
+
+import Mailsh.Message
+
+main :: IO ()
+main = do
+  i <- BS.getContents
+  case parse (message mime) i of
+    Left err -> error ("Error parsing message: " ++ err)
+    Right msg -> do
+      BL.putStrLn (renderMessage msg)
